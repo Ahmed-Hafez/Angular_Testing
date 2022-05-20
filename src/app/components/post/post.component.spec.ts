9de02e65100d67a3012250b8a2e466a9fa20099c
Ahmed-Hafez <ahmed160035@feng.bu.edu.eg>
2022-05-20 14:00:51 +0200

    Starting in posts exmple

---
+++ src/app/components/post/post.component.spec.ts
@@ -0,0 +1,32 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { first } from 'rxjs/operators';
+import { Post } from 'src/app/models/post.model';
+
+import { PostComponent } from './post.component';
+
+describe('Post Component', () => {
+  let fixture: ComponentFixture<PostComponent>;
+  let comp: PostComponent;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PostComponent],
+    });
+
+    fixture = TestBed.createComponent(PostComponent);
+    comp = fixture.componentInstance;
+  });
+
+  it('should create post component using TestBed', () => {
+    expect(comp).toBeDefined();
+  });
+
+  it('should raise an event when the delete post is clicked', () => {
+    const post: Post = { id: 1, body: 'body 1', title: 'dsdsd' };
+    comp.post = post;
+    comp.delete.pipe(first()).subscribe((selectedPost) => {
+      expect(selectedPost).toEqual(post);
+    });
+
+    comp.onDeletePost(new MouseEvent('click'));
+  });
+});
---
+++ src/app/components/posts/posts.component.spec.ts
@@ -0,0 +1,82 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { Post } from 'src/app/models/post.model';
+import { PostService } from 'src/app/services/post.service';
+
+import { PostsComponent } from './posts.component';
+
+const POSTS = [
+  {
+    id: 1,
+    body: 'body 1',
+    title: 'title 1',
+  },
+  {
+    id: 2,
+    body: 'body 2',
+    title: 'title 2',
+  },
+  {
+    id: 3,
+    body: 'body 3',
+    title: 'title 3',
+  },
+];
+
+class mockPostService {
+  getPosts() {
+    return of(POSTS);
+  }
+
+  deletePost(post: Post) {
+    return of(true);
+  }
+}
+
+describe('Posts Component', () => {
+  let component: PostsComponent;
+  let postService: any;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        PostsComponent,
+        {
+          provide: PostService,
+          useClass: mockPostService,
+        },
+      ],
+    });
+
+    component = TestBed.inject(PostsComponent);
+    postService = TestBed.inject(PostService);
+  });
+
+  describe('get', () => {
+    it('should get posts when getPosts method is called', () => {
+      expect(component.posts.length).toEqual(0);
+      component.getPosts();
+      expect(component.posts.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe('delete', () => {
+    beforeEach(() => {
+      component.posts = POSTS;
+    });
+    it('should delete the selected Post from the posts', () => {
+      component.deletePost(POSTS[1]);
+
+      expect(component.posts.length).toBe(2);
+      for (let post of component.posts) {
+        expect(post).not.toEqual(POSTS[1]);
+      }
+    });
+
+    it('should call the delete method in Post Service only once', () => {
+      spyOn(postService, 'deletePost').and.callThrough();
+      component.deletePost(POSTS[1]);
+      expect(postService.deletePost).toHaveBeenCalledTimes(1);
+    });
+  });
+});
